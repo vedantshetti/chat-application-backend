64ff4afd5c397e089d23f1862381b22bb73c040d
vedantshetti <vedantshetti123456@gmail.com>
2025-04-01 17:08:08 +0530

    added the logic of registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+
 export const register = async (req, res) => {
     try {
         const {fullName, username, password, confirmPassword, gender} = req.body;
+        if(!fullName || !username || !password || !confirmPassword || !gender){
+            return res.status(400).json({ message: 'Please fill all the fields' }); 
+        };
+        if(password !== confirmPassword){
+            return res.status(400).json({ message: 'Passwords do not match' });
+        };  
+
+        const user = await UserActivation.findOne({ where: { username } });
+        if (user) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        await User.create({
+            fullName,
+            username,
+            password: hashedPassword,
+            profilePhoto:gender === male? maleProfilePhoto : femaleProfilePhoto,
+        });
         
         
     } catch (error) {
