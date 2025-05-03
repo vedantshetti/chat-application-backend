8bb2641300764b0c9aff73c3c9c0109a6f8e7644
vedantshetti <vedantshetti123456@gmail.com>
2025-05-03 12:30:18 +0530

    added the login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,80 @@
-import { User } from '../models/userModel.js';
-import bcrypt from 'bcryptjs';
+import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    try {
-        const {fullName, username, password, confirmPassword, gender} = req.body;
-        if(!fullName || !username || !password || !confirmPassword || !gender){
-            return res.status(400).json({ message: 'Please fill all the fields' }); 
-        };
-        if(password !== confirmPassword){
-            return res.status(400).json({ message: 'Passwords do not match' });
-        };  
-
-        const user = await User.findOne({ username });
-        if (user) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-        const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
-
-        await User.create({
-            fullName,
-            username,
-            password: hashedPassword,
-            gender,
-            profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
-        });
-        
-        res.status(201).json({ message: 'User registered successfully' });
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ message: "Please fill all the fields" });
     }
-}
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    await User.create({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      fullName: user.fullName,
+      profilePhoto: user.profilePhoto,
+    };
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, { maxage: 24 * 60 * 60 * 1000 })
+      .json({ message: "Login successful", token, user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
